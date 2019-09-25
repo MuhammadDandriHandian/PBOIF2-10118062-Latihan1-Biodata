4ac6c8af3ab42bbb432e43808495748d1e54e9ff
MuhammadDandriHandian <55798941+MuhammadDandriHandian@users.noreply.github.com>
2019-09-25 21:31:57 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118062/latihan1/biodata/PBOIF210118062Latihan1Biodata.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118062.latihan1.biodata;
+
+/**
+ *
+ * @author 
+ * Nama     : Muhammad Dandri Handian
+ * Kelas    : IF-2
+ * NIM      : 10118062
+ */
+public class PBOIF210118062Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "----------------");
+        System.out.println("NAMA\t\t: Muhammad Dandri Handian");
+        System.out.println("ALAMAT\t\t: Gelatik Dalam");
+        System.out.println("TEMPAT LAHIR\t: Cianjur");
+        System.out.println("TANGGAL LAHIR\t: 26 Mei 2000");
+        System.out.println("--------------");
+        System.out.println("HOBI\t\t: Olahraga, bermusik, membaca");
+        System.out.println("MAKES\t\t: Sate");
+        System.out.println("MIKES\t\t: Susu");
+    }
+    
+}
